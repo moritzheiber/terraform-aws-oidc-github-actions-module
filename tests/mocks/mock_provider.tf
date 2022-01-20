fd40b01b15108d49933fd9da208c038bfacf5e8f
Moritz Heiber <hello@heiber.im>
2022-01-20 14:40:29 +0100

    Ignore tfsec warnings about embedded secrets in the mock provider

--- tests/mocks/mock_provider.tf
+++ tests/mocks/mock_provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  access_key                  = "mock_access_key"
+  access_key                  = "mock_access_key" #tfsec:ignore:aws-misc-no-exposing-plaintext-credentials
   region                      = "eu-central-1"
   s3_force_path_style         = true
-  secret_key                  = "mock_secret_key"
+  secret_key                  = "mock_secret_key" #tfsec:ignore:general-secrets-sensitive-in-attribute
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
